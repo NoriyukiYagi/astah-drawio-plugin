e5fcb456d4be59b78902f20f89cc0a699f354554
Noriyuki Yagi <noriyuki-yagi@mamezou.com>
2025-04-24 12:32:10 +0900

    Fix frame layout

--- src/main/java/astah_drawio_plugin/internal/mxgraph/builder/node/MxGraphFrameBuilder.java
+++ src/main/java/astah_drawio_plugin/internal/mxgraph/builder/node/MxGraphFrameBuilder.java
@@ -4,11 +4,17 @@ import com.change_vision.jude.api.inf.presentation.INodePresentation;
 
 import astah_drawio_plugin.internal.annotation.GraphElementBuilder;
 import astah_drawio_plugin.internal.mxgraph.builder.base.MxGraphNodeBuilder;
+import astah_drawio_plugin.internal.utils.TextGeometryCalculator;
 
 @GraphElementBuilder(astahTypes = "Frame")
 public class MxGraphFrameBuilder extends MxGraphNodeBuilder {
+	private static final int LABEL_WIDTH_MARGIN = 20;
+
 	public MxGraphFrameBuilder(MxGraphNodeBuilder parent, INodePresentation p) {
 		super(parent, p);
+		this.setStereotypesVisible(false);
+		this.setValue(p.getLabel());
+		var labelWidth = TextGeometryCalculator.getTextWidth(this.getLabel());
 		this.addStyle("shape", "umlFrame");
 		this.addStyle("whiteSpace", "wrap");
 		this.addStyle("html", "1");
@@ -16,8 +22,6 @@ public class MxGraphFrameBuilder extends MxGraphNodeBuilder {
 		this.addStyle("recursiveResize", "0");
 		this.addStyle("container", "1");
 		this.addStyle("collapsible", "0");
-		this.addStyle("width", "160");
-		this.setValue(p.getLabel());
-		this.setStereotypesVisible(false);
+		this.addStyle("width", String.valueOf(labelWidth + LABEL_WIDTH_MARGIN));
 	}
 }
